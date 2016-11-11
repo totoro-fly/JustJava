206ebc0ebe23ca8d1c51c96c8a3d9a11557b54da
totoro-fly <liujian180389@gmail.com>
2016-11-11 13:30:18 +0800

    增加displayPrice 测试崩溃

--- app/src/main/java/com/totoro_fly/justjava/MainActivity.java
+++ app/src/main/java/com/totoro_fly/justjava/MainActivity.java
@@ -1,15 +1,17 @@
 package com.totoro_fly.justjava;
+
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 /**
-        *Add your package below.Package name can be found in the project's AndroidManifest.xml file.
-        *This is the package name our example uses:
-        *
-        *package com.example.android.justjava;
-        */
+ * Add your package below.Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ * <p>
+ * package com.example.android.justjava;
+ */
 
 /**
  * This app displays an order form to order coffee.
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        int sum=0;
+        int sum = 0;
         setContentView(R.layout.activity_main);
     }
 
@@ -27,13 +29,12 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        TextView quantityTextView= (TextView) findViewById(R.id.quantity_text_view);
-        int a= Integer.parseInt(String.valueOf(quantityTextView.getText()));
-        a++;
-        display(a);
-
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        int quantity = Integer.parseInt(String.valueOf(quantityTextView.getText()));
+        quantity++;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
-
 
     /**
      * This method displays the given quantity value on the screen.
@@ -41,5 +42,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method display the given price on the screen
+     */
+    private void displayPrice(int number) {
+        TextView pricetextview = (TextView) findViewById(R.id.price_text_view);
+        pricetextview.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
